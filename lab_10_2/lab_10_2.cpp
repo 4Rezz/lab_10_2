90850f6de91a2fc2e12b694cb11ee0d67325eb30
Roman Ilyshun <romanilyshun@gmail.com>
2021-05-29 14:13:30 +0300

    lab10_2

---
+++ lab_10_2/lab_10_2.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+int checkTXT();
+
+
+int main()
+{
+	cout << "Words with same first and last letter: " << checkTXT() << endl;
+}
+
+
+int checkTXT() 
+{ 
+	ifstream fin("t.txt"); // відкрили файл для зчитування
+	char s[100]; // прочитаний рядок
+	char* w; // виокремлене з рядка слово
+	int k = 0; // кількість слів
+	while (fin.getline(s, sizeof(s))) // поки можна прочитати рядок
+	{
+		w = strtok(s, " .,:;!?-'"); // початок циклу виокремлення слів
+		while (w != NULL) // поки можна виокремити слово
+		{
+			int d = strlen(w);
+			if (w[0] == w[d-1])	{
+				k++;
+			}
+			w = strtok(NULL, " .,:;!?-'"); // виокремлюємо наступне слово
+		}
+	}
+	fin.close();
+	return k;
+}
